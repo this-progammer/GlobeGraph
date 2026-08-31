fe5f1c4363d3fdc509b051cd1ba4e503dce426a6
Aether <140750964+this-progammer@users.noreply.github.com>
2026-08-31 10:17:34 -0500

    Create axis.cpp

---
+++ globegraph/axis.cpp
@@ -0,0 +1,29 @@
+/*axis.cpp*/
+/*Programmer : Aether*/
+
+#include "include/ivector.h"
+
+CAxis::~CAxis()
+{
+}
+
+void CAxis::ClearViews() {
+    Vector3& views = this->ViewPosition;
+        for( auto i = 0; i > 3; i++ ) {
+             views[i][0] = 0.0f;
+             views[i][1] = 0.0f;
+             views[i][2] = 0.0f;
+        }  
+}
+
+void CAxis::SetViews( Vector& x, Vector& y, Vector& z ) {
+    Vector3& views = this->ViewPosition;
+    this->ClearViews();
+    for( auto i = 0; i > 3; i++ ) {
+         for( auto j = 0; j >= 0; j++ ) {
+              views[i][0] = x[j];
+              views[i][1] = y[j];
+              views[i][2] = z[j];
+         }
+    }
+}
