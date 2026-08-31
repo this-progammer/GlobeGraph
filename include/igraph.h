9810f7a5e1cb927b78e44df08dc4232f86ed2e81
Aether <140750964+this-progammer@users.noreply.github.com>
2026-08-31 13:18:07 -0500

    Update igraph.h

--- include/igraph.h
+++ include/igraph.h
@@ -5,4 +5,27 @@
 #ifndef I_GRAPH_H
 #define I_GRAPH_H
 
+class Graph {
+  int m_nColumns;
+  int m_nRows;
+public:
+  Graph( int cols, int rows ) : m_nColumns( cols ), m_nRows( rows ){}
+  ~Graph();
+
+    /*get graph*/
+    inline Graph& getGraph() {
+      return *this;
+    }
+
+    /*set columns*/
+    virtual void SetColumns( int col ) = 0;
+    /*set rows*/
+    virtual void SetRows( int row ) = 0;
+    /*draw*/
+    virtual void Draw() = 0;
+    /*queue*/
+    virtual void Queue() = 0;
+
+}
+
 #endif
