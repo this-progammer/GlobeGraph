80564ad64c8dcf5ac61bc2f279f1ccc0260cf89f
Aether <140750964+this-progammer@users.noreply.github.com>
2026-08-31 10:03:26 -0500

    Update ivector.h

--- include/ivector.h
+++ include/ivector.h
@@ -6,6 +6,10 @@
 #ifndef I_VECTOR_H
 #define I_VECTOR_H
 
-
+typedef float Vector;
+typedef Vector Vector2[2];
+typedef Vector Vector3[3];
+typedef Vector Vector4[4];
+typedef Vector Vector6[6];
 
 #endif
