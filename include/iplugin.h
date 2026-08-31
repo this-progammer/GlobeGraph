23f1e9d9bde64496300522546bb027132f6f2752
Aether <140750964+this-progammer@users.noreply.github.com>
2026-08-31 08:19:38 -0500

    Update iplugin.h

--- include/iplugin.h
+++ include/iplugin.h
@@ -10,4 +10,21 @@
 /*Get Plugin Name*/
 typedef const char*(PFN_GET_PLUGIN_NAME)(const char* IPLUGIN);
 
+class IPlugin {
+  const char* m_Plugin;
+public:
+  IPlugin( const char* plugin ) : m_Plugin( plugin ){}
+  ~IPlugin();
+
+  /*get the plugin*/
+  inline IPlugin& getPlugin() {
+      return *this;
+  }
+
+  /*connect to API*/
+  virtual void ConnectToAPI() = 0;
+  /*Disconnect to API*/
+  virtual void DisconnectToAPI() = 0;
+};
+
 #endif
