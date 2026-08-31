6b62c164a2fa98cfd6ce2d325811d9c9dafbade7
Aether <140750964+this-progammer@users.noreply.github.com>
2026-08-31 10:09:48 -0500

    Update ivector.h

--- include/ivector.h
+++ include/ivector.h
@@ -12,4 +12,31 @@ typedef Vector Vector3[3];
 typedef Vector Vector4[4];
 typedef Vector Vector6[6];
 
+class CAxis {
+  int ViewType;
+  Vector3 ViewPosition;
+public:
+  CAxis( int vt, Vector3& v ) : ViewType( vt ), ViewPosition( v ){}
+  ~CAxis();
+
+  /*get axis*/
+  inline CAxis& getAxis() {
+      return *this;
+  }
+
+  /*get viewtype*/
+  const int getViewType() const {
+      return this->ViewType;
+  }
+
+  /*get view position*/
+  const Vector3& getView() const {
+      return this->ViewPosition;
+  }
+
+  /*clear the views*/
+  virtual void ClearViews() = 0;
+
+};
+
 #endif
