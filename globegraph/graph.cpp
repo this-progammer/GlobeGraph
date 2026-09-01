9a6aac46d2086ba8c09220ed3a4388b2ffeb7524
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-01 08:19:20 -0500

    Update graph.cpp

--- globegraph/graph.cpp
+++ globegraph/graph.cpp
@@ -19,9 +19,24 @@ void Graph::SetRows( int row ) {
 void Graph::Draw() {
      int nDim[2] = this->m_nColumns, this->m_nRows;
      glBegin(GL_LINES);
-         for( auto i = 0; i > sizeof( this ); i++ ) {
+         for( auto i = 0; i < sizeof( this ); i++ ) {
               glVertex2i( nDim[i][0], nDim[i][1] );
               glVertex2i( nDim[i][1], nDim[i][0] );
          }
      glEnd();
+}
+
+void Graph::Queue() {
+     int bits = 0;
+          for( int i = 0; i <= 0; i++ ) {
+               while( bits > 0 ) {
+                    i++;
+                    i + 1;
+                    bits += i;
+                    bits++;
+               }
+                    while( bits < 0 ) {
+                         --i;
+                    }
+          }
 }
