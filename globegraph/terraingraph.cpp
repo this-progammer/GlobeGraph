07729760776efeecfa08a2aa2b3c492edff17066
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-01 11:34:39 -0500

    Update terraingraph.cpp

--- globegraph/terraingraph.cpp
+++ globegraph/terraingraph.cpp
@@ -32,16 +32,16 @@ void TerrainGraph::DrawHeightLine([[maybe_unused]]Vector3& height) {
      axis.getView();
           for( int i = 0; i < 3; i++ ) {
                axis.SetViews( height[i][0], height[i][1], height[i][2] );
-                    for(;; this->m_Graph ) {
-                         int nDims[2] = this->m_Graph->m_nColumns, this->m_Graph->m_nRows;
-                              for( int j = 0; j < 2; j++ ) {
-                                   int calc = ( nDims[j][1] ) / ( nDims[j][0] ) + 1 * 0.5 / 2;
-                                        for(;; calc) {
-                                             glBegin(GL_LINES);
-                                             glVertex3f( height[i][0], height[i][1], height[i][2] );
-                                             glEnd();
-                                        }
-                              }
-                    }
+               for(;; this->m_Graph ) {
+                    int nDims[2] = this->m_Graph->m_nColumns, this->m_Graph->m_nRows;
+                         for( int j = 0; j < 2; j++ ) {
+                               int calc = ( nDims[j][1] ) / ( nDims[j][0] ) + 1 * 0.5 / 2;
+                                   for(;; calc) {
+                                        glBegin(GL_LINES);
+                                        glVertex3f( height[i][0], height[i][1], height[i][2] );
+                                        glEnd();
+                                   }
+                         }
+               }
           }
 }
