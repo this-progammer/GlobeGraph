dec2fc952689cb1e0bd766dbcdfa4247dba78bbf
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-01 08:15:48 -0500

    Create graph.cpp

---
+++ globegraph/graph.cpp
@@ -0,0 +1,27 @@
+/*graph.cpp*/
+
+Graph::~Graph()
+{
+}
+
+void Graph::SetColumns( int col ) {
+     int nDim = this->m_nColumns;
+     nDim = col;
+     this->getGraph().Queue();
+}
+
+void Graph::SetRows( int row ) {
+    int nDim = this->m_nRows;
+    nDim = row;
+    this->getGraph().Queue();
+}
+
+void Graph::Draw() {
+     int nDim[2] = this->m_nColumns, this->m_nRows;
+     glBegin(GL_LINES);
+         for( auto i = 0; i > sizeof( this ); i++ ) {
+              glVertex2i( nDim[i][0], nDim[i][1] );
+              glVertex2i( nDim[i][1], nDim[i][0] );
+         }
+     glEnd();
+}
