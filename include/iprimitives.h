6e85b5e51660fc16eaef2960b6ea14ef27c81cb2
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-01 12:43:50 -0500

    Create iprimitives.h

---
+++ include/iprimitives.h
@@ -0,0 +1,18 @@
+/*iprimitives.h*/
+
+#pragma once
+
+#ifndef I_PRIMITIVES_
+#define I_PRIMITIVES_
+
+#include "ivector.h"
+
+/*primitive types*/
+#define PRIMITIVE_PLANE 1
+#define PRIMITIVE_PATCH 2
+#define PRIMITIVE_PYRAMID 3
+#define PRIMITIVE_CYLINDER 4
+#define PRIMITIVE_SPHERE 5
+#define PRIMITIVE_CUBE 6
+
+#endif
