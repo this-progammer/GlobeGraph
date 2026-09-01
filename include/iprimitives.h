2f35169898457f85098086030f520c9673d03684
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-01 12:52:37 -0500

    Update iprimitives.h

--- include/iprimitives.h
+++ include/iprimitives.h
@@ -15,4 +15,26 @@
 #define PRIMITIVE_SPHERE 5
 #define PRIMITIVE_CUBE 6
 
+class IPrimitive {
+    Vector3 m_Mins;
+    Vector3 m_Maxs;
+    int m_Type;
+    Vector3 m_Color;
+    Vector m_fScale;
+    const std::string m_Name;
+public:
+    IPrimitive( Vector3& mins, Vector3& maxs, int id, Vector3& color, Vector scale, const std::string& name ) : m_Mins( mins ), m_Maxs( maxs ), m_Type( id ), m_Color( color ), m_fScale( scale ), m_Name( name ){}
+    ~IPrimitive();
+
+    inline IPrimitive& getPrimitive() {
+           return *this;
+    }
+
+    virtual void CreatePrimitive( Vector3& mins, Vector3& maxs, int id, Vector3& color, Vector scale, const std::string& name ) = 0;
+    virtual void DestroyPrimitive() = 0;
+    virtual void Draw() = 0;
+    virtual void SetMinsMaxs( Vector3& fMins, Vector3& fMaxs ) = 0;
+    
+};
+
 #endif
