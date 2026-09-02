d364e7ce559978ca6c53fafd4902d6921de0e8e0
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-02 12:49:25 -0500

    Update cube.hpp

--- globegraph/cube.hpp
+++ globegraph/cube.hpp
@@ -6,4 +6,11 @@ class Cube : public IPrimitive {
 public:
       Cube( Vector3& mins, Vector3& maxs, Vector3& color ) : m_Mins( mins ), m_Maxs( maxs ), m_Color( color ){}
       ~Cube();
+
+      inline Cube& getCube() {
+            return *this;
+      }
+
+      virtual void CreateCube() = 0;
+      virtual void DestroyCube() = 0;
 };
