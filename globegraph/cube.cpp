712536f1f25ddfd8d8cb502671db2d2f1bf6e306
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-02 13:02:47 -0500

    Update cube.cpp

--- globegraph/cube.cpp
+++ globegraph/cube.cpp
@@ -1,2 +1,33 @@
 /*cube.cpp*/
 
+#include "cube.hpp"
+
+Cube::~Cube()
+{
+}
+
+void Cube::DestroyCube() {
+    this->~Cube();
+}
+
+void Cube::CreateCube() {
+     Cube& cube = nullptr;
+     cube.getCube();
+     Vector3& mins = this->m_Mins;
+     Vector3& maxs = this->m_Maxs;
+     Vector3& color = this->m_Color;
+       for( auto i = cube.begin(); i != cube.end(); i++ ) {
+            cube.SetType( PRIMITIVE_CUBE );
+               for( auto j = 0; j < 3; j++ ) {
+                    cube.CreatePrimitive( mins[i], maxs[i], color[i], PRIMITIVE_CUBE, this->getScale(), this->getName() );
+                    cube.SetColor( color[i] );
+                    const std::string n = this->m_Name;
+                    cube.SetName( n );
+                         for( auto k = 0; k < 6; k++ ) {
+                                 for(;; k) {
+                                     cube.AddFace(); // add 6 faces...
+                                 }
+                         }
+               }
+       }
+}
