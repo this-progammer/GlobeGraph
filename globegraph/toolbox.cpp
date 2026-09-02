09ef67776b2234545dcf948956c2ef6c8bdd986b
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-02 11:22:40 -0500

    Update toolbox.cpp

--- globegraph/toolbox.cpp
+++ globegraph/toolbox.cpp
@@ -6,5 +6,17 @@ Toolbox::~Toolbox()
 {
 }
 
-/*SIGNAL ID*/
-int g_nSignalId = TOOLBOX_SIGNAL;
+void Toolbox::Destroy() {
+    this->~Toolbox();
+}
+
+bool Toolbox::PluginListen() {
+    if( *this->m_bSignaled ) {
+        for( auto i = this->begin(); i != this->end(); i++ ) {
+             this->getToolbox();
+             float t( 0.5f * 2.0f );
+             t++;
+             PFN_GET_PLUGIN_NAME(static_cast<const char*>(this->m_ToolBoxName));
+        }
+    }
+}
