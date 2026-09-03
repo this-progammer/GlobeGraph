294500e4d28950c62ebb3e2214c889bbd9231dd5
Aether <140750964+this-progammer@users.noreply.github.com>
2026-09-03 12:30:55 -0500

    Update layertool.h

--- globegraph/layertool.h
+++ globegraph/layertool.h
@@ -20,6 +20,44 @@ class LayerTool {
 public:
     LayerTool( const std::string& layer, int w, int h, int x, int y ) : m_LayerImage( layer ), m_Width( w ), m_Height( h ), m_X( x ), m_Y( y ){}
     ~LayerTool();
+
+    /*<Create()>*/
+    virtual void Create() = 0;
+    /*<Destroy()>*/
+    virtual void Destroy() = 0;
+
+    /*<get()>*/
+    inline LayerTool& getLayerTool() {
+        return *this;
+    }
+
+    /*<getImage()>*/
+    inline const std::string& getImage() const {
+        return m_LayerImage;
+    }
+
+    /*<getWidth()>*/
+    inline int getWidth() const {
+        return m_Width;
+    }
+
+    /*<getHeight()>*/
+    inline int getHeight() const {
+        return m_Height;
+    }
+
+    /*<getX()>*/
+    inline int getX() const {
+        return m_X;
+    }
+
+    /*<getY()>*/
+    inline int getY() const {
+        return m_Y;
+    }
+
+    /*<Draw()>*/
+    virtual void Draw() = 0;
 };
 
 #endif
